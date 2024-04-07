a6130a17d9904bd0d3f3ba474b69edf1b8209516
Josh-Tang112 <jiixiit@gmail.com>
2024-04-07 18:51:47 -0400

    compilable version

--- physics/physics.hpp
+++ physics/physics.hpp
@@ -41,7 +41,7 @@ struct PhysicsSolver{
         // calculate the dim of surface
         int num_edges = 0;
         for(int i = 0; i < in.num_surface; i++){
-            num_edges += in.surf[0];
+            num_edges += in.surf[i][0];
         }
         num_edges += 4; // for the 4 edges of the window
 
@@ -76,7 +76,8 @@ struct PhysicsSolver{
             }
         }
         // adding the 4 edges of the window to the data structures appropriately
-        int window_up[2][2] = {{0,0},{in.width,0}}, window_left[2] = {{0,0},{0, in.height}}, window_down[2][2] = {{0, in.height}, {in.width, in.height}}, window_right[2][2] = {{in.width,0},{in.width, in.height}};
+        int window_up[2][2] = {{0,0},{in.width,0}}, window_left[2][2] = {{0,0},{0, in.height}};
+        int window_down[2][2] = {{0, in.height}, {in.width, in.height}}, window_right[2][2] = {{in.width,0},{in.width, in.height}};
         std::memcpy(bbs + num_edges - 4, window_up, 4 * sizeof(int));
         edges[num_edges - 4] = {{{0,0},{in.width,0}}, {{1, 0},{0,-1}}};
         std::memcpy(bbs + num_edges - 3, window_left, 4 * sizeof(int));
@@ -86,7 +87,7 @@ struct PhysicsSolver{
         std::memcpy(bbs + num_edges - 1, window_right, 4* sizeof(int));
         edges[num_edges - 1] = {{{0,0},{in.width,0}}, {{-1, 0},{0,1}}};
 
-        this->surface_tree = STRTREE(bbs, edges, num_edges);
+        this->surface_tree = STRTREE<struct Edge, 12>((int***)bbs, edges, num_edges);
 
         for(int i = 0; i < in.num_ball; i ++){
             this->pos.push_back(sf::Vector2f(((int (*)[2])in.pos)[i][0],((int (*)[2])in.pos)[i][1]));
@@ -118,24 +119,24 @@ struct PhysicsSolver{
     }
 
     // calculate the distance between 2 points
-    int point_point_distance(int p1[2], int p2[2]){
-        int xdiff = p1[0] - p2[0], ydiff = p1[1] - p2[1];
+    double point_point_distance(float p1[2], float p2[2]){
+        float xdiff = p1[0] - p2[0], ydiff = p1[1] - p2[1];
         return xdiff * xdiff + ydiff * ydiff;
     }
 
     // calculate shortest squared distance from C to AB and find the closest point
-    float point_line_dist(int A[2], int B[2], int C[2], float closest[2]){
-        int CA[2] = {C[0] - A[0], C[1] - A[1]}, AB = {B[0] - A[0], B[1] - A[1]};
-        float scalar = ((float)(CA[0] * AB[0] + CA[1] * AB[1])) / (AB[0] * AB[0] + AB[1] * AB[1]);
+    double point_line_dist(float A[2], float B[2], float C[2], float closest[2]){
+        float CA[2] = {C[0] - A[0], C[1] - A[1]}, AB[2] = {B[0] - A[0], B[1] - A[1]};
+        float scalar = (CA[0] * AB[0] + CA[1] * AB[1]) / (AB[0] * AB[0] + AB[1] * AB[1]);
         if(scalar > 0 && scalar < 1){
             closest[0] = AB[0] * scalar;
             closest[1] = AB[1] * scalar;
         }
         else if(scalar <= 0){
-            std::memcpy(closest,A,2 * sizeof(int));
+            std::memcpy(closest,A,2 * sizeof(float));
         }
         else {
-            std::memcpy(closest,B,2 * sizeof(int));
+            std::memcpy(closest,B,2 * sizeof(float));
         }
         float p[2] = {C[0] - closest[0], C[1] - closest[1]};
         return p[0] * p[0] + p[1] * p[1];
@@ -145,47 +146,69 @@ struct PhysicsSolver{
     // which can be tested by testing whether (CD X CA) and (CD X CB), (AB X AC) and (AB X AD) has different signs
     // to account for AB intersecting at C or D, cross product being zero will be counted as intersecting
     // we only need to compute the k component, which is just a determinant
-    bool intersect(int A[2], int B[2], int C[2], int D[2]){
-        int CD[2] = {D[0] - C[0], D[1] - C[1]}, CA[2] = {A[0] - C[0], A[1] - C[1]}, CB[2] = {B[0] - C[0], B[1] - C[1]};
-        int CDXCA = CD[0] * CA[1] - CD[1] * CA[0], CDXCB = CD[0] * CB[1] - CD[1] * CB[0];
+    bool intersect(float A[2], float B[2], float C[2], float D[2]){
+        float CD[2] = {D[0] - C[0], D[1] - C[1]}, CA[2] = {A[0] - C[0], A[1] - C[1]}, CB[2] = {B[0] - C[0], B[1] - C[1]};
+        float CDXCA = CD[0] * CA[1] - CD[1] * CA[0], CDXCB = CD[0] * CB[1] - CD[1] * CB[0];
         if(CDXCA * CDXCB > 0){return false;}
         else if(CDXCA * CDXCB == 0){return true;}
 
-        int AB[2] = {B[0] - A[0], B[1] - A[1]}, AC[2] = {C[0] - A[0], C[1] - A[1]}, AD[2] = {D[0] - A[0], D[1] - A[1]};
-        int ABXAC = AB[0] * AC[1] - AB[1] * AC[0], ABXAD = AB[0] * AD[1] - AB[1] * AD[0];
+        float AB[2] = {B[0] - A[0], B[1] - A[1]}, AC[2] = {C[0] - A[0], C[1] - A[1]}, AD[2] = {D[0] - A[0], D[1] - A[1]};
+        float ABXAC = AB[0] * AC[1] - AB[1] * AC[0], ABXAD = AB[0] * AD[1] - AB[1] * AD[0];
         if(ABXAC * ABXAD > 0){return false;}
         else{return true;}
     }
 
+    // find the intersection of two lines AB and VW, and return whether the intersection is on line segments AB and VW
+    // we find the intersection using parameterization and determinant
+    bool intersection(float A[2], float B[2], float V[2], float W[2], float intersection[2]){
+        float s,t;
+        float a = W[0] - V[0], b = A[0] - B[0], c = W[1] - V[1], d = A[1] - B[1], e = A[0] - V[0], f = A[1] - V[1];
+        float det = a * d - b * c;
+        if(!det){return false;}
+        s = (e * d - b * f) / det;
+        t = (a * f - c * e) / det;
+        intersection[0] = A[0] - t * b;
+        intersection[1] = A[1] - t * d;
+
+        return (s >= 0 && s <= 1) && (t >= 0 && t <= 1);
+    }
+
     void posUpdate(sf::Vector2f accel, float collision_damping){
-        sf::Vector2f tmp;
+        int r = this->cell_dim / 2;
         for(unsigned int i = 0; i < this->pos.size(); i++){
             std::vector<struct Branch<struct Edge> *> *query_res;
-            int q[2][2];
+            int q[2][2], bb[2][2];
             
             // update velocity
             this->vel[i] += accel * this->delta_time;
+
+            // calculate a vector parallel to the velocity vector with size r
+            float vel_size = std::sqrt(this->vel[i].x * this->vel[i].x + this->vel[i].y * this->vel[i].y); 
+            float direction[2] = {this->vel[i].x * r / vel_size, this->vel[i].y * r / vel_size};
             
             // preparing query
             sf::Vector2f old_pos = this->pos[i], new_pos = this->pos[i] + this->vel[i] * this->delta_time;
-            int points[2][2] = {{old_pos.x, old_pos.y}, {new_pos.x,new_pos.y}}, bb[2][2];
-            get_bounding_box(points, bb);
+            float points[2][2] = {{old_pos.x, old_pos.y}, {new_pos.x,new_pos.y}};
+            get_bounding_box((int (*)[2])points, bb);
 
             // get query result
             query_res = this->surface_tree.query(bb);
 
             struct Edge e;
+            float intersect[2];
             for(auto b : *query_res){
                 e = b->data;
-                if(intersect(points[0], points[1], e.points[0], e.points[1])){
-
+                if(intersection(points[0], points[1], (float*)e.points[0], (float*)e.points[1], intersect)){
+                    this->pos[i] = sf::Vector2f(intersect[0] - direction[0], intersect[1] - direction[1]);
+                    float vx = this->vel[i].x, vy = this->vel[i].y;
+                    this->vel[i].x = collision_damping * (vx * e.mat[0][0] + vy * e.mat[0][1]);
+                    this->vel[i].y = collision_damping * (vx * e.mat[1][0] + vy * e.mat[1][1]);
+                    break;
+                }
+                else{
+                    this->pos[i] = sf::Vector2f(new_pos);
                 }
             }
-            
-            if(min_dist == (unsigned int)-1){continue;}
-            float vx = this->vel[i].x, vy = this->vel[i].y;
-            this->vel[i].x = collision_damping * (vx * ((float (*)[2])matrix)[0][0] + vy * ((float (*)[2])matrix)[0][1]);
-            this->vel[i].y = collision_damping * (vx * ((float (*)[2])matrix)[1][0] + vy * ((float (*)[2])matrix)[1][1]);
             
             delete query_res;
         }
